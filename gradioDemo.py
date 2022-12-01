9ddba49bfe9d9a79b9140e9f626df8440486e35a
Eric Shields <shieldse01@yahoo.com>
2022-11-30 21:19:17 -0600

    Gradio Update

--- gradioDemo.py
+++ gradioDemo.py
@@ -7,10 +7,25 @@ def output(inputs):
     r = requests.post(url=URL, json={"inputs": inputs}).content
     return r
 
-demo = gr.Interface(
-    fn=output,
-    inputs=["text"],
-    outputs=["text"]
-)
+with gr.Blocks() as demo:
+    gr.Markdown("Classify Text or FAQ")
+    with gr.Tab("Classification"):
+        classification_input = gr.Textbox(label="Input")
+        classification_output = gr.Textbox(label="Output")
+        classification_button = gr.Button("Classify")
+    with gr.Tab("FAQ"):
+        faq_input = gr.Textbox(label="Input")
+        faq_output = gr.Textbox(label="Output")
+        faq_button = gr.Button("Get FAQ")
+
+    classification_button.click(
+        output, 
+        inputs=classification_input, 
+        outputs=classification_output)
+    faq_button.click(
+        output,
+        inputs=faq_input,
+        outputs=faq_output
+    )
 
 demo.launch()
